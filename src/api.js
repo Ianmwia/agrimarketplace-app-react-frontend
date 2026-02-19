4c269cd70cb964ca130ba47984bd6bdc84fea57d
Mwia <iansnr10@gmail.com>
2026-02-19 12:39:45 +0300

    adding django credential tokens for authorization and authentication

--- src/api.js
+++ src/api.js
@@ -4,6 +4,11 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
+    withCredentials: true,
 });
+
+{/*Auto handle django csrf tokens */}
+API.defaults.xsrfCookieName = 'csrftoken';
+API.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 export default API;
