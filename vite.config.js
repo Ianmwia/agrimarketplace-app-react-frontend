a75cc36bce0b55321e68392bba4f4e8d4074a83b
Mwia <iansnr10@gmail.com>
2026-02-20 11:01:27 +0300

    installing shadcn and it additional files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+import path from "path"
+import { fileURLToPath } from "url"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Replicate __dirname logic
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
+  optimizeDeps: {
+    include: ['recharts'],
+  },
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
