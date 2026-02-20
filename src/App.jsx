6a4fac6ad41484735c83e09e6d1d8a4fffd4d53e
Mwia <iansnr10@gmail.com>
2026-02-20 10:58:09 +0300

    adding route layout for navbar pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from './components/ui/sonner'
 import Login from '../pages/LoginForm'
 import SignUp from '../pages/SignupForm'
+import Navbar from '../components/Navbar'
+import Layout from './components/layout'
+import Home from '../pages/Home'
 
 function App() {
   
   return (
     <BrowserRouter>
+    <Toaster position='top-right'/>
       <Routes>
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<SignUp/>} />
+        {/*navbar routes */}
+        <Route element={<Layout />}>
+        <Route path='/home' element={<Home/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
