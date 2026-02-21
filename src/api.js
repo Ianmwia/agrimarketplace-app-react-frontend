d1fb96ae2dedbe114d6aed172a314a20f2ff0716
Mwia <iansnr10@gmail.com>
2026-02-21 17:27:07 +0300

    adding inteceptor to get token for requests from django before crud calls

--- src/api.js
+++ src/api.js
@@ -3,12 +3,34 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "http://localhost:8000/api/",
     withCredentials: true,
 });
 
 {/*Auto handle django csrf tokens */}
 API.defaults.xsrfCookieName = 'csrftoken';
 API.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+{/*add interceptor to get cookies */}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+API.interceptors.request.use(async (config) => {
+    if (['post', 'put', 'patch', 'delete'].includes(config.method)) {
+        let csrfToken = getCookie('csrftoken');
+
+        if (csrfToken){
+            await axios.get('http://localhost:8000/api/csrf/', { withCredentials: true });
+            csrfToken = getCookie('csrftoken');
+        }
+
+        config.headers['X-CSRFToken'] = csrfToken;
+    }
+    return config;
+});
 
 export default API;
