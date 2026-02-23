602373d2542b9660a3670e4d38fef988e5f57bfb
Mwia <iansnr10@gmail.com>
2026-02-23 12:00:10 +0300

    creating a dialog box to reject orders for suctomers

---
+++ dashboards/farmerspages/RejectDialogBox.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import API from '@/api';
+import { toast } from 'sonner';
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
+
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+
+export default function RejectDialog(){
+
+}
