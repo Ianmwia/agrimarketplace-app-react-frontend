609271121c27ab4b5a435f24358df42c782a1f06
Mwia <iansnr10@gmail.com>
2026-02-25 08:48:09 +0300

    created a dialog box for farmers to add a rejection reason for purchases made on their products

--- dashboards/farmerspages/RejectDialogBox.jsx
+++ dashboards/farmerspages/RejectDialogBox.jsx
@@ -7,6 +7,55 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 
-export default function RejectDialog(){
+export default function RejectDialog({orderId, open,setOpen, refresh}){
+    const [reason, setReason] = useState('')
+
+    const handleSubmit = async ()=> {
+        if (!reason.trim()){
+            toast.error('enter a rejection reason')
+            return
+        }
+
+        try {
+            await API.post(`order/${orderId}/reject/`, {reason})
+            toast.success('order Rejected')
+            setOpen(false)
+            setReason('')
+            refresh()
+        } catch (error) {
+            toast.error('Failed to reject Order')
+        }
+    }
+
+    return(
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Reject Order #{orderId} for {orderId.name} from {orderId.first_name} {orderId.last_name}</DialogTitle>
+                </DialogHeader>
+                <div>
+                    <Textarea
+                    placeholder='enter a reason for rejecting'
+                    value={reason}
+                    onChange={(e) => setReason(e.target.value)}
+                    >
+
+                    </Textarea>
+                </div>
+                <DialogFooter>
+                    <Button variant='outline'
+                    onClick={() => setOpen(false)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button variant='destructive'
+                    onClick={() => handleSubmit}
+                    >
+                        Reject
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
 
 }
