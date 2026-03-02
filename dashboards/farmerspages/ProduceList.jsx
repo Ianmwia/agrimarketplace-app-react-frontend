4d21f0e1011d931013c0849c6734f8bcf9ca3110
Mwia <iansnr10@gmail.com>
2026-03-02 08:42:36 +0300

    styling the farmers produce listing page

--- dashboards/farmerspages/ProduceList.jsx
+++ dashboards/farmerspages/ProduceList.jsx
@@ -11,6 +11,8 @@ export default function ProduceList(){
     const fetchProduce = async () => {
         try {
             const res = await API.get('produce/')
+            //console.log('data', res.data)
+            //console.log('data', res.data.produce_list)
             setProduce(res.data.produce_list || [])
         } catch (error) {
             toast.error('failed to load produce')
@@ -24,7 +26,7 @@ export default function ProduceList(){
     return(
         <div className='space-y-4'>
             <div>
-                <p className='text-xl font-bold mb-6 text-center'> My Produce Listings</p>
+                <p className='text-xl font-bold mb-6 text-center'> My Harvest Listings</p>
             </div>
 
             {produce.length === 0 ? (
@@ -44,31 +46,53 @@ export default function ProduceList(){
                             </div>
                             <CardHeader className='p-5 border-b bg-muted/30'>
                             <div className='flex justify-between items-start gap-2'>
-                            <CardTitle className='text-lg font-bold'>
-                                Create New Produce
+                            <CardTitle>
+                                <div>
+                                    <p className='text-sm text-muted-foreground'>Name</p>
+                                    <p className='text-lg font-bold'>{item.name}</p>
+                                </div>
+                                
                             </CardTitle>
-                            <div className='text-xl tracking-tighter'>
-                                Ksh {item.price}
+                            <div>
+                                <div>
+                                    <p className='text-sm text-muted-foreground'>Category</p>
+                                    <p className='text-lg font-bold'>{item.category}</p>
+                                </div>
                             </div>
                             </div>
                             </CardHeader>
 
                             <CardContent className='p-5 space-y-1'>
-                                <div className='text-sm space-y-1'>
-                                    <p>
-                                <span className='text-muted-foreground font-semibold uppercase tracking-wider text-[10px]'>
-                                    Available: 
-                                </span>{" "}
-                                {item.quantity} KG
-                                    </p>
-                                    <p>
-                                <span className='text-muted-foreground font-semibold uppercase tracking-wider text-[10px]'>
-                                    Available: 
-                                </span>{" "}
-                                {item.quantity > 0 ? "In Stock" : "Out Of Stock"}
-                                    </p>
+                                <div className='flex gap-2  justify-between'>
+                                    <p className='text-sm text-muted-foreground'>Farmer</p>
+                                    <p className='text-lg font-bold'>{item.farmer_name}</p>
                                 </div>
-
+                                <div className='flex gap-2 justify-between'>
+                                    <p className='text-sm text-muted-foreground'>Description:</p>
+                                    <p className='text-lg font-bold'>{item.description}</p>
+                                </div>
+                                {item.batches && item.batches.length > 0 ? (
+                                    <div className='space-y-3 mt-3 '>
+                                        {item.batches.map(batch =>(
+                                            <div key={batch.id} className='rounded-md'>
+                                                <div className='flex justify-between'>
+                                                    <p className='text-sm text-muted-foreground'>Batch Number:</p>
+                                                    <p className='text-lg font-bold'>#{batch.batch_number}</p>
+                                                </div>
+                                                <div className='flex gap-2 justify-between'>
+                                                    <p className='text-sm text-muted-foreground'>Quantity:</p>
+                                                    <p className='text-lg font-semibold'>{batch.quantity} KG</p>
+                                                </div>
+                                                <div className='flex gap-2 justify-between'>
+                                                    <p className='text-sm text-muted-foreground'>Price:</p>
+                                                    <p className='text-lg font-bold'>Ksh {batch.price_per_unit}</p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ):(
+                                    <p className='text-xl py-8'>No batches Available</p>
+                                )}
                             </CardContent>
                         </Card>
                     ))}
@@ -76,6 +100,7 @@ export default function ProduceList(){
             )}
             
         </div>
+        
     )
     
 }
