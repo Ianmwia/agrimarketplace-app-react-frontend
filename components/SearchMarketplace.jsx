1385304501214d1651a6e85ab37283ad9174c2f8
Mwia <iansnr10@gmail.com>
2026-04-07 11:05:36 +0300

    update searchbar

--- components/SearchMarketplace.jsx
+++ components/SearchMarketplace.jsx
@@ -15,7 +15,7 @@ export default function SearchMarketplace({onSearch}){
     }, [searchTerm, onSearch])
 
     return (
-        <div className='flex flex-col md:flex-row gap-4 mb-8 md:w-[47%] md:mx-auto lg:w-[38%] bg-card border rounded-xl shadow-sm'>
+        <div className='flex flex-col md:flex-row gap-4 mb-8 md:w-[47%] md:mx-auto lg:w-full bg-card border rounded-xl shadow-sm'>
                 <Input
                 value={searchTerm}
                 placeholder="search by produce name or location..."
