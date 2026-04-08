366f8fee0276085300cf5dbd8e8409b49a2ff374
Mwia <iansnr10@gmail.com>
2026-04-08 09:08:23 +0300

    add logo icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -105,10 +105,12 @@ export default function Navbar({onSearch, showSearch}){
             {/*brand web name */}
             {!user ?( 
             <div className={`absolute left-1/2 -translate-x-1/2 transition-all duration-300`}>
+                <img className='h-8 w-8' src="/public/farmer-svgrepo-com.svg" alt="logo-kilimo" />
                 <Link to='/' className='text-4xl font-bold tracking-tighter'>Kilimo</Link>
             </div>
             ): (
-                <div className='lg:order-1 lg:ml-auto'>
+                <div className='lg:order-1 lg:ml-auto flex items-center gap-1'>
+                <img className='h-8 w-8' src="/public/farmer-svgrepo-com.svg" alt="logo-kilimo" />
                 <p className='text-4xl font-bold tracking-tighter'>Kilimo</p>
                 </div>
             )}
