657b79602467e965f37f8161bfe16bc8e19869de
Mwia <iansnr10@gmail.com>
2026-04-08 18:36:31 +0300

    update user profile

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -118,7 +118,7 @@ export default function Profile(){
                                         <p className='text-lg font-medium'>{user?.role}</p>
                                     </div>
 
-                                    <div className='pt-6 flex gap-3'>
+                                    <div className='pt-6 flex gap-3 sm:flex-col'>
                                         <Button
                                             onClick={()=> setIsEditing(true)}
                                             className='w-full md:w-auto px-10'
