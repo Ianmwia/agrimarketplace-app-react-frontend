a7b4ee0aed72f4efa5bc88572f2c3b67507f01c2
Mwia <iansnr10@gmail.com>
2026-04-08 08:59:29 +0300

    handle navbar state changes

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../../components/Navbar';
+import { useState } from 'react';
 
 const Layout = () => {
+
+    const [searchQuery, setSearchQuery] = useState('')
     return(
         <div className='flex min-h-screen flex-col'>
-        <Navbar />
+        <Navbar showSearch={true} onSearch={setSearchQuery}/>
             <main className='flex-1'>
-                <Outlet/>
+                <Outlet context={{searchQuery, setSearchQuery}}/>
             </main>
         </div>
     )
