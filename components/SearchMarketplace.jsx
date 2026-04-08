7d0b64a5123aa89385b070cbcefe4257d2a59b74
Mwia <iansnr10@gmail.com>
2026-04-08 10:17:59 +0300

    add search icon

--- components/SearchMarketplace.jsx
+++ components/SearchMarketplace.jsx
@@ -15,9 +15,11 @@ export default function SearchMarketplace({onSearch}){
     }, [searchTerm, onSearch])
 
     return (
-        <div className='flex lg:items-center lg:my-4 flex-col md:flex-row gap-4 mb-8 md:w-[47%] md:mx-auto lg:w-3xs bg-card border rounded-xl shadow-sm'>
+        <div className='relative flex lg:items-center lg:my-4 flex-col md:flex-row gap-4 mb-8 md:w-[47%] md:mx-auto lg:w-3xs bg-card border rounded-xl shadow-sm'>
+                <Search className='absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground pointer-events-none z-10'/>
                 <Input
                 value={searchTerm}
+                className='pl-8 border-none bg-transparent focus-visible:ring-0 shadow-none'
                 placeholder="search by produce name or location..."
                 onChange = {(e)=> setSearchTerm(e.target.value)}
                 
